bf2234088acbefaace54fd133d40cc5dba8c8903
Zach Crandell <zachariah@pm.me>
2019-01-15 07:52:23 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php include '_includes/header.php';?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+          <h4 class="my-4">2019 Top Tech
+            <small>by crandell/io</small>
+          </h1>
+
+
+<?php include '_includes/articles.php';?>
+
+<?php include '_includes/page.php';?>
+
+        </div>
+
+<?php include '_includes/widgets.php';?>        
+
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+<?php include '_includes/footer.php';?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
